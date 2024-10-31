5117b04488e4b05ce34faa901d6028d2c5ff538c
ffuerst-dev <felix.fuerst2002@outlook.com>
2024-10-31 15:49:03 -0600

    tested the heading and the button text

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,18 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [myRecipes, setMyRecipes] = useState(null)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Jest</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increment Count
-        </button>
-        <p>Count is: {count}</p>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  if(!myRecipes)
+    return (
+      <>
+        <div>
+          <h2>My Recipes</h2>
+          <button>Add Recipe</button>
+          <p>There are no recipes to list</p>
+        </div>
+      </>
+    );
 }
 
 export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,39 +8,15 @@ describe(`App`, () => {
     render(<App />);
   });
 
-  it(`initializes with text 'Vite + React + Jest'`, () => {
-    // Arrange
-    const heading = screen.getByRole("heading");
-    // Act
-    // Assert
-    expect(heading).toHaveTextContent("Vite + React + Jest");
-  });
+  it('should show My Recipes as a header', () => {
+    const header = screen.getByRole('heading');
 
-  it(`initializes counter at 0`, () => {
-    // Arrange
-    const counterText = screen.getByText(/Count is: /i);
-    // Act
-    // Assert
-    expect(counterText).toHaveTextContent("Count is: 0");
-  });
+    expect(header).toHaveTextContent('My Recipes');
+  })
 
-  it(`increments counter on button click`, () => {
-    // Arrange
-    const button = screen.getByRole("button");
-    const counterText = screen.getByText(/Count is: /i);
-    // Act
-    fireEvent.click(button);
-    // Assert
-    expect(counterText).toHaveTextContent("Count is: 1");
-  });
+  it('button should exist', () => {
+    const button = screen.getByRole('button');
+    expect(button).toHaveTextContent('Add Recipe');
+  })
 
-  it(`should fail, as the test is bad`, () => {
-    // Arrange
-    const button = screen.getByRole("button");
-    const counterText = screen.getByText(/Count is: /i);
-    // Act
-    fireEvent.click(button);
-    // Assert
-    expect(counterText).toHaveTextContent("Count is: pikachu");
-  });
 });
